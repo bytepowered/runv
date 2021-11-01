61c416e3acba48fc0b46998151df743f6b5bc45b
陈哈哈 <yongjia.chen@hotmail.com>
2021-11-01 20:22:10 +0800

    Update context.go

--- context.go
+++ context.go
@@ -3,24 +3,58 @@ package runv
 import (
 	"context"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
-type Context interface {
-	context.Context
-	ValueE(key interface{}) (value interface{}, ok bool)
-	Log() *logrus.Logger
+var _ Context = new(StateContext)
+
+type StateContext struct {
+	ctx    context.Context
+	vars   map[interface{}]interface{}
+	logger *logrus.Logger
 }
 
-type Initable interface {
-	OnInit() error
+func NewStateContext(ctx context.Context, logger *logrus.Logger, vars map[interface{}]interface{}) *StateContext {
+	if vars == nil {
+		vars = make(map[interface{}]interface{}, 0)
+	}
+	return &StateContext{ctx: ctx, logger: logger, vars: vars}
 }
 
-type StateComponent interface {
-	Setup(ctx context.Context) Context
-	Serve(Context) error
+func NewStateContext1(ctx context.Context, vars map[interface{}]interface{}) *StateContext {
+	return NewStateContext(ctx, nil, vars)
 }
 
-type Component interface {
-	Startup(ctx context.Context) error
-	Shutdown(ctx context.Context) error
+func NewStateContext2(ctx context.Context) *StateContext {
+	return NewStateContext(ctx, nil, nil)
+}
+
+func (s *StateContext) Deadline() (deadline time.Time, ok bool) {
+	return s.ctx.Deadline()
+}
+
+func (s *StateContext) Done() <-chan struct{} {
+	return s.ctx.Done()
+}
+
+func (s *StateContext) Err() error {
+	return s.ctx.Err()
+}
+
+func (s *StateContext) Value(key interface{}) interface{} {
+	v, _ := s.ValueE(key)
+	return v
+}
+
+func (s *StateContext) ValueE(key interface{}) (value interface{}, ok bool) {
+	value, ok = s.vars[key]
+	if ok {
+		return value, true
+	}
+	value = s.ctx.Value(key)
+	return value, nil == value
+}
+
+func (s *StateContext) Log() *logrus.Logger {
+	return s.logger
 }
