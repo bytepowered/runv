c863774187c4f1bb08a8e76844e4aa5cf66a8fa3
陈哈哈 <yongjia.chen@hotmail.com>
2021-11-01 16:55:48 +0800

    Update compb.go

--- sample/compb.go
+++ sample/compb.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/bytepowered/runv"
 	"github.com/sirupsen/logrus"
 )
@@ -14,23 +16,12 @@ func (c *CompB) SetCompA(a *CompA) {
 	logrus.Info("setup: " + a.Name())
 }
 
-func (c *CompB) OnInit() error {
-	logrus.Info("on init: B")
-
+func (c *CompB) Startup(ctx context.Context) error {
+	fmt.Println("startup: B")
 	return nil
 }
 
-func (c *CompB) Startup(ctx runv.Context) error {
-	ctx.Log().Infof("startup: B")
-	return nil
-}
-
-func (c *CompB) Serve(ctx runv.Context) error {
-	ctx.Log().Infof("serve: B")
-	return nil
-}
-
-func (c *CompB) Shutdown(ctx runv.Context) error {
-	ctx.Log().Infof("shutdown: B")
+func (c *CompB) Shutdown(ctx context.Context) error {
+	fmt.Println("shutdown: B")
 	return nil
 }
