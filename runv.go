61f5dfda7ae9563ca4f2035f1c8e61f36f39f4ec
yongjiachen <yongjia.chen@hotmail.com>
2021-11-08 22:17:27 +0800

    fix

--- runv.go
+++ runv.go
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	app = &wrapper{
+	w = &wrapper{
 		logger:     logrus.New(),
 		initables:  make([]Initable, 0, 4),
 		components: make([]Component, 0, 4),
@@ -45,36 +45,25 @@ func (w *wrapper) SetLogger(logger *logrus.Logger) {
 	w.logger = logger
 }
 
-func init() {
-	inject.RegisterProvider(&logrus.Logger{
-		Out:          os.Stderr,
-		Formatter:    new(logrus.JSONFormatter),
-		Hooks:        make(logrus.LevelHooks),
-		Level:        logrus.DebugLevel,
-		ExitFunc:     os.Exit,
-		ReportCaller: false,
-	})
-}
-
 func SetAppAwaitFunc(saf func() <-chan os.Signal) {
 	appAwait = saf
 }
 
 // AddPrepareHook 添加PrepareHook函数
 func AddPrepareHook(hook func() error) {
-	app.prehooks = append(app.prehooks, hook)
+	w.prehooks = append(w.prehooks, hook)
 }
 
 // AddPostHook 添加PostHook函数
 func AddPostHook(hook func() error) {
-	app.posthooks = append(app.posthooks, hook)
+	w.posthooks = append(w.posthooks, hook)
 }
 
 // Provider 添加Prototype对象的Provider函数
 func Provider(providerFunc interface{}) {
 	inject.RegisterProvider(providerFunc)
 	// update app deps
-	inject.ResolveDeps(app)
+	inject.ResolveDeps(w)
 }
 
 // Add 添加单例组件
@@ -85,41 +74,41 @@ func Add(obj interface{}) {
 	hits := 0
 	if init, ok := obj.(Initable); ok {
 		hits++
-		app.initables = append(app.initables, init)
+		w.initables = append(w.initables, init)
 	}
 	if comp, ok := obj.(Component); ok {
 		hits++
-		app.components = append(app.components, comp)
+		w.components = append(w.components, comp)
 	}
 	if state, ok := obj.(StateComponent); ok {
 		hits++
-		app.states = append(app.states, state)
+		w.states = append(w.states, state)
 	}
 	if hits == 0 {
 		panic(fmt.Errorf("app: add an unsupported component, type: %T ", obj))
 	}
-	app.refs = append(app.refs, obj)
+	w.refs = append(w.refs, obj)
 	inject.RegisterObject(obj)
 	// update app deps
-	inject.ResolveDeps(app)
+	inject.ResolveDeps(w)
 }
 
 func RunV() {
 	// prepare hooks
-	for _, prehook := range app.prehooks {
+	for _, prehook := range w.prehooks {
 		if err := prehook(); err != nil {
-			app.logger.Fatalf("app: prepare hook error: %s", err)
+			w.logger.Fatalf("app: prepare hook error: %s", err)
 		}
 	}
 	// inject deps
-	for _, obj := range app.refs {
+	for _, obj := range w.refs {
 		inject.ResolveDeps(obj)
 	}
-	app.logger.Infof("app: init")
+	w.logger.Infof("app: init")
 	// init
-	for _, obj := range app.initables {
+	for _, obj := range w.initables {
 		if err := obj.OnInit(); err != nil {
-			app.logger.Fatalf("app: init error: %s", err)
+			w.logger.Fatalf("app: init error: %s", err)
 		}
 	}
 	goctx, ctxfun := context.WithCancel(context.Background())
@@ -128,37 +117,37 @@ func RunV() {
 	defer shutdown(goctx)
 	// startup
 	if err := startup(goctx); err != nil {
-		app.logger.Fatalf("app startup, error: %s", err)
+		w.logger.Fatalf("app startup, error: %s", err)
 	}
 	// serve with setup
 	if err := serve(goctx); err != nil {
-		app.logger.Fatalf("app serve, error: %s", err)
+		w.logger.Fatalf("app serve, error: %s", err)
 	}
 	// post hook
-	for _, posthook := range app.posthooks {
+	for _, posthook := range w.posthooks {
 		if err := posthook(); err != nil {
-			app.logger.Fatalf("app: post hook error: %s", err)
+			w.logger.Fatalf("app: post hook error: %s", err)
 		}
 	}
-	app.logger.Infof("app: run, waiting signals...")
+	w.logger.Infof("app: run, waiting signals...")
 	<-appAwait()
 }
 
 func shutdown(goctx context.Context) {
-	defer app.logger.Infof("app: terminaled")
-	for _, obj := range app.components {
-		ctx := NewStateContext(goctx, app.logger, nil)
+	defer w.logger.Infof("app: terminaled")
+	for _, obj := range w.components {
+		ctx := NewStateContext(goctx, w.logger, nil)
 		err := metric2(ctx, fmt.Sprintf("component[%T] shutdown...", obj), obj.Shutdown)
 		if err != nil {
-			app.logger.Errorf("shutdown error: %s", err)
+			w.logger.Errorf("shutdown error: %s", err)
 		}
 	}
 }
 
 func startup(goctx context.Context) error {
-	app.logger.Infof("app: startup")
-	for _, obj := range app.components {
-		ctx := NewStateContext(goctx, app.logger, nil)
+	w.logger.Infof("app: startup")
+	for _, obj := range w.components {
+		ctx := NewStateContext(goctx, w.logger, nil)
 		err := metric2(ctx, fmt.Sprintf("component[%T] startup...", obj), obj.Startup)
 		if err != nil {
 			return fmt.Errorf("[%T] startup error: %s", obj, err)
@@ -168,11 +157,11 @@ func startup(goctx context.Context) error {
 }
 
 func serve(goctx context.Context) error {
-	app.logger.Infof("app: serve")
-	for _, state := range app.states {
+	w.logger.Infof("app: serve")
+	for _, state := range w.states {
 		ctx := state.Setup(goctx)
 		if statectx, ok := ctx.(*StateContext); ok && statectx.logger == nil {
-			statectx.logger = app.logger
+			statectx.logger = w.logger
 		}
 		err := metric1(ctx, fmt.Sprintf("component[%T] start serve...", state), state.Serve)
 		if err != nil {
